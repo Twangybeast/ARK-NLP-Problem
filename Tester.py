b019297ac15af5aa921a213d6a370f04e4554833
Twangybeast <twangybeast@gmail.com>
2019-04-19 21:46:13 -0700

    Tried increasing REPLACE accuracy (only about 60-70%) - Attempted speeding up Spacy (Way to slow)

--- Tester.py
+++ Tester.py
@@ -1,15 +1,18 @@
 import csv
 import random
+import time
 import unicodedata
 
 import ErrorClassifier
 
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+import en_core_web_lg
+nlp = en_core_web_lg.load()
 
 from ErrorClassifier import ERROR_TYPES, tokenize, tokenize_pure_words
 
 word_freqs = {}
+
+
 def load_word_frequencies():
     with open('learned_frequencies.csv') as file:
         reader = csv.reader(file)
@@ -18,20 +21,30 @@ def load_word_frequencies():
             freq = int(row[1])
             word_freqs[word] = freq
 
+
+def eval_largest(n1, n2):
+    if n1 == n2:
+        return int(random.random() * 2)  # default to random
+    elif n1 > n2:
+        return 0
+    else:
+        return 1
+
+
 def solve_ARRANGE(p1, p2):
-    return int(random.random() * 2) # default to random
+    return int(random.random() * 2)  # default to random
+
 
 # Addition/Removal are symmetrical operations. In the testing dataset, they should be treated the same
 def solve_ADD(p1, p2):
     return 1 - solve_REMOVE(p2, p1)
 
+
 def solve_REMOVE(p1, p2):
     # default behavior, return the larger one (removal of tokens from larger one is common)
     t1, t2 = tokenize(p1, p2)
-    if len(t1) > len(t2):
-        return 0
-    else:
-        return 1
+    return eval_largest(len(t1), len(t2))
+
 
 def solve_TYPO(p1, p2):
     # Find the delta
@@ -50,11 +63,17 @@ def solve_TYPO(p1, p2):
                 total += 1
         # don't bother using an average
         return total
+
     # Assume the one with higher frequencies is correct
-    if calc_freq_sum(w1) > calc_freq_sum(w2):
-        return 0
-    else:
-        return 1
+    return eval_largest(calc_freq_sum(w1), calc_freq_sum(w2))
+
+
+def check_all_has_vectors(doc):
+    for d in doc:
+        if not d.has_vector:
+            return False
+    return True
+
 
 def evaluate_average_delta_similarity(delta_tokens, starting_match, ending_match):
     delta = ' '.join(delta_tokens)
@@ -69,31 +88,15 @@ def evaluate_average_delta_similarity(delta_tokens, starting_match, ending_match
 
     total = 0.0
     for d in d_tokens:
-        for t in s_tokens:
-            total += d.similarity(t)
-        for t in e_tokens:
-            total += d.similarity(t)
+        if d.has_vector:
+            for t in s_tokens:
+                total += d.similarity(t) if t.has_vector else 0
+            for t in e_tokens:
+                total += d.similarity(t) if t.has_vector else 0
 
-    return total / size_d
+    return total / size_d, check_all_has_vectors(d_tokens)
 
-    '''
-    i = 0;
-    found_d = False
-    while i < len(p):
-        p_t = p[i]
-        if p_t.text == d[i].text:
-            assert not found_d
-            found_d = True
-            while i < len(p):
-                p_t = p[i]
-                if p_t.text != d[i].text:
-                    i -= 1 # For the subsequent i += 1 in the outer loop
-                    break
-                i += 1
-        i += 1
-    '''
-
-
+asdf = [[0, 0, 0, 0], [0,0,0,0]]
 def solve_REPLACE(p1, p2):
     # Simplest method, simply compare word similarity vectors
     # Find the delta
@@ -106,30 +109,30 @@ def solve_REPLACE(p1, p2):
     s1 = ' '.join(s1)
     s2 = ' '.join(s2)
 
-    #in_d1 = ErrorClassifier.all_in_words_list(d1)
-    #in_d2 = ErrorClassifier.all_in_words_list(d2)
+    # in_d1 = ErrorClassifier.all_in_words_list(d1)
+    # in_d2 = ErrorClassifier.all_in_words_list(d2)
 
     ##if in_d1 and in_d2:
     #    pass
 
     # greater similarity is better
-    sim1 = evaluate_average_delta_similarity(d1, s1, s2)
-    sim2 = evaluate_average_delta_similarity(d2, s1, s2)
+    sim1, c1 = evaluate_average_delta_similarity(d1, s1, s2)
+    sim2, c2 = evaluate_average_delta_similarity(d2, s1, s2)
 
-    if sim1 > sim2:
-        return 0
-    else:
-        return 1
-    #return int(random.random() * 2)  # default to random
+    res = eval_largest(sim1, sim2)
+    asdf[res][(2 if c1 else 0) + (1 if c2 else 0)] += 1
+    return res
+
 
 def solve(p1, p2, error_type):
-    return globals()['solve_'+error_type](p1, p2)
-
+    return globals()['solve_' + error_type](p1, p2)
 
 
 prediction_freq = [0, 0]
 error_correct = {k: 0 for k in ERROR_TYPES}
 error_freq = {k: 0 for k in ERROR_TYPES}
+
+TESTING_RANGE = (900000, 1000000)
 
 # loads model data
 load_word_frequencies()
@@ -138,7 +141,17 @@ with open('train.txt', encoding='utf-8') as fin:
     progress = 0
     for line in fin:
         progress += 1
-        if progress < 900000:
+        if not (TESTING_RANGE[0] < progress <= TESTING_RANGE[1]):
+            continue
+
+    fin.seek(0)
+
+    progress = 0
+    start_time = time.time()
+    words_processed = 0
+    for line in fin:
+        progress += 1
+        if not (TESTING_RANGE[0] < progress <= TESTING_RANGE[1]):
             continue
 
         line = line.strip()
@@ -152,9 +165,12 @@ with open('train.txt', encoding='utf-8') as fin:
             error_correct[error_type] += 1
         error_freq[error_type] += 1
 
-        #Display progression in number of samples processed, use random to avoid too many (slow) interactions w/ console
-        if random.random() < 0.001:
-            print('\rProgress: [{0}]'.format(progress), end='')
+        # Display progression in number of samples processed, use random to avoid too many (slow) interactions w/
+        # console
+        words_processed += len(p1.split()) + len(p2.split())
+        if random.random() < 0.01:
+            print('\rProgress: [{0}] Word Processed: [{1}] Words per second: [{2}]'
+                  .format(progress, words_processed, (words_processed / (time.time() - start_time))), end='')
 
 print(prediction_freq)
 print(error_correct)
--- Trainer.py
+++ Trainer.py
@@ -1,28 +1,38 @@
 import csv
 import os
 import random
+import time
 import unicodedata
+
+import en_core_web_lg
+nlp = en_core_web_lg.load()
 
 import ErrorClassifier
 from ErrorClassifier import ERROR_TYPES, tokenize_pure_words
 
-
 # Only can learn when the learned_words.txt file is empty
-LEARN_WORDS_ENABLED = False
+ENABLE_LEARN_WORDS = False
+ENABLE_TRAIN_EMBEDDINGS = True
 
 learned_words = set()
+
+
 def learn_words(part):
     words = tokenize_pure_words(part)
     for word in words:
         if not ErrorClassifier.check_word_list(word):
             learned_words.add(word)
 
+
 def save_learned_words():
     with open('learned_words.txt', 'w') as fout:
         for word in learned_words:
             fout.write(word + '\n')
 
+
 word_freqs = {}
+
+
 def learn_word_frequencies(part):
     words = tokenize_pure_words(part)
     for word in words:
@@ -30,6 +40,7 @@ def learn_word_frequencies(part):
             word_freqs[word] += 1
         else:
             word_freqs[word] = 1
+
 
 def save_word_frequencies():
     with open('learned_frequencies.csv', 'w', newline='') as fout:
@@ -40,34 +51,83 @@ def save_word_frequencies():
                 csv_writer.writerow([word, freq])
 
 
+word_to_id = {}
+# TODO consider advanced tokenization
+def learn_embeddings(part):
+    words = tokenize_pure_words(part)
+    for word in words:
+        pass
+
+
+
 
 def train(p1, p2, error_type):
     # note: learn words is done in the error classifier (classification requires knowing the words)
-    learn_word_frequencies(p1) # only train frequencies on first part, second part is corrupted text
-    if LEARN_WORDS_ENABLED:
+    learn_word_frequencies(p1)  # only train frequencies on first part, second part is corrupted text
+    if ENABLE_LEARN_WORDS:
         learn_words(p1)
     pass
 
 
-with open('train.txt', encoding='utf-8') as fin:
-    progress = 0
-    for line in fin:
-        progress += 1
-        if progress > 900000:
-            break
+if __name__ == '__main__':
+    TESTING_RANGE = (900000, 1000000)
+    with open('train.txt', encoding='utf-8') as fin:
+        progress = 0
+        words_processed = 0
+        start_time = time.time()
+        parts_list = []
+        for line in fin:
+            progress += 1
+            if TESTING_RANGE[0] < progress <= TESTING_RANGE[1]:
+                break
 
-        line = line.strip()
-        line = unicodedata.normalize('NFKD', line)
-        p1, p2 = line.split('\t')
-        error_type = ErrorClassifier.classify_error_labeled(p1, p2)
-        train(p1, p2, error_type)
+            line = line.strip()
+            line = unicodedata.normalize('NFKD', line)
+            p1, p2 = line.split('\t')
 
-        #Display progression in number of samples processed, use random to avoid too many (slow) interactions w/ console
-        if random.random() < 0.001:
-            print('\rProgress: [{0}]'.format(progress), end='')
+            words_processed += len(p1.split()) + len(p2.split())
 
-if LEARN_WORDS_ENABLED:
-    save_learned_words()
-else:
-    assert len(learned_words) == 0
-save_word_frequencies()
+            parts_list.append(p1)
+            parts_list.append(p2)
+
+        fin.seek(0)
+        print('Time: {0}\nWPS: {1}'.format(time.time() - start_time, words_processed/(time.time()-start_time)))
+        nlp_tokens_iter = nlp.pipe(parts_list, batch_size=1000).__iter__()
+        print('----')
+        print('----')
+        print('----')
+        start_time = time.time()
+        progress = 0
+        words_processed = 0
+        for x in nlp_tokens_iter:
+            progress += 1
+            words_processed += len(x)
+            if random.random() < 0.01:
+                print('\rProgress: {0} Words: {1} WPS: {2}'.format(progress,  words_processed, words_processed / (time.time() - start_time)), end='')
+
+        print('Time: {0}\nWPS: {1}'.format(time.time() - start_time, words_processed/(time.time()-start_time)))
+
+
+
+        progress = 0
+        for line in fin:
+            progress += 1
+            if TESTING_RANGE[0] < progress <= TESTING_RANGE[1]:
+                break
+
+            line = line.strip()
+            line = unicodedata.normalize('NFKD', line)
+            p1, p2 = line.split('\t')
+            error_type = ErrorClassifier.classify_error_labeled(p1, p2)
+            train(p1, p2, error_type)
+
+            # Display progression in number of samples processed, use random to avoid too many (slow) interactions w/
+            # console
+            if random.random() < 0.001:
+                print('\rProgress: [{0}]'.format(progress), end='')
+
+    if ENABLE_LEARN_WORDS:
+        save_learned_words()
+    else:
+        assert len(learned_words) == 0
+    save_word_frequencies()
